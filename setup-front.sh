932b0a9bcd88181b991bfd9f95e5bb760ce57bb0
Leandro Nunes <dr1design@gmail.com>
2015-11-04 09:42:33 -0200

    Update setup-front.sh

--- setup-front.sh
+++ setup-front.sh
@@ -2,52 +2,71 @@
 
 red=`tput setaf 1`
 green=`tput setaf 2`
+blue=`tput setaf 4`
 nc=`tput sgr0` # No Color
 
 
-# EDITOR #
+echo "${blue}## EDITOR ##${nc}"
 sublime=`which subl`
 if [ subl = "" ]; then
   echo "${red}✖ ${nc}Installing Sublime..."
   apt-get install sublime-text
-else
-  echo "${green}✔ ${nc}Sublime is already installed"
 fi
+echo "${green}✔ ${nc}Sublime is installed"
 
 
-# NODE #
+echo "${blue}## SCM ##${nc}"
+git=`which git`
+if [ "$git" = "" ]; then
+  echo "${red}✖ ${nc}Installing Git..."
+  apt-get install git
+fi
+echo "${green}✔ ${nc}Git is installed"
+
+svn=`which svn`
+if [ "$svn" = "" ]; then
+  echo "${red}✖ ${nc}Installing SVN..."
+  apt-get install subversion
+fi
+echo "${green}✔ ${nc}SVN is installed"
+
+
+echo "${blue}## NODE (modules) ##${nc}"
 node=`which node`
 if [ "$node" = "" ]; then
-  echo "${red}✖ ${nc}NodeJS must be installed!"
+  echo "${red}✖ ${nc}NodeJS must be (manually) installed! Check nodejs.org${nc}"
   exit 1
-else
-  echo "${green}✔ ${nc}Node is already installed"
 fi
+echo "${green}✔ ${nc}Node is installed"
+
 package=`npm list -g gulp | grep ERR`
 if [ "$package" != "" ]; then
   echo "${red}✖ ${nc} |__ Installing gulp..."
   npm install -g gulp
-else
-  echo "${green}✔ ${nc} |__ Gulp is already installed"
 fi
+echo "${green}✔ ${nc} |__ Gulp is installed"
+
 package=`npm list -g grunt | grep ERR`
 if [ "$package" != "" ]; then
   echo "${red}✖ ${nc} |__ Installing grunt..."
   npm install -g grunt-cli
-else
-  echo "${green}✔ ${nc} |__ Grunt is already installed"
 fi
+echo "${green}✔ ${nc} |__ Grunt is installed"
+
 package=`npm list -g bower | grep ERR`
 if [ "$package" != "" ]; then
   echo "${red}✖ ${nc} |__ Installing bower..."
   npm install -g bower
-else
-  echo "${green}✔ ${nc} |__ Bower is already installed"
 fi
+echo "${green}✔ ${nc} |__ Bower is installed"
+
 package=`npm list -g yo | grep ERR`
 if [ "$package" != "" ]; then
   echo "${red}✖ ${nc} |__ Installing yo..."
   npm install -g yo
-else
-  echo "${green}✔ ${nc} |__ yo is already installed"
 fi
+echo "${green}✔ ${nc} |__ yo is installed"
+
+echo "${green}Everything is installed. EVERYTHING !"
+echo "\$\$\$---  Show me the code!!!  ---\$\$\$${nc}"
+echo ""
